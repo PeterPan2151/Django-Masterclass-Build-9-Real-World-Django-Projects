a58058deef8365e9d7f2de7315c30dd3d2c09e21
Salvador Montiel <126306642+PeterPan2151@users.noreply.github.com>
2025-06-22 18:55:34 -0600

    Add files via upload

--- mysite/food/views.py
+++ mysite/food/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Item
+from django.template import loader
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello World')
+    item_list = Item.objects.all()
+    template = loader.get_template('food/index.html')
+    context = {
+
+    }
+    return HttpResponse(template.render(context, request))
 
 def items(request):
     return HttpResponse('This is an item view')
