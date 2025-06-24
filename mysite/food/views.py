ba7081bb4d3852c37b692bb268fb7928b2636957
Salvador Montiel <126306642+PeterPan2151@users.noreply.github.com>
2025-06-23 22:29:29 -0600

    Add files via upload

--- mysite/food/views.py
+++ mysite/food/views.py
@@ -6,11 +6,13 @@ from django.template import loader
 # Create your views here.
 def index(request):
     item_list = Item.objects.all()
-    template = loader.get_template('food/index.html')
     context = {
-
+        'item_list': item_list,
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'food/index.html', context)
 
 def items(request):
     return HttpResponse('This is an item view')
+
+def detail(request, item_id):
+    return HttpResponse('This is item no/id: ' % item_id)
